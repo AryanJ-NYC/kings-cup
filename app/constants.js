95b4da93f56d1507e2faa00eb623ab3840152b80
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2017-09-16 20:39:06 -0400

    Add Rules

--- app/constants.js
+++ app/constants.js
@@ -1,3 +1,57 @@
 export const RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"];
 export const SUITS = ["S", "H", "C", "D"];
 
+export const RULES = {
+  "A": {
+    name: "Waterfalls",
+    description: "To perform a waterfall, each player starts drinking their beverage at the same time as the person to their left. No player can stop drinking until the player before them stops.",
+  },
+  "2": {
+    name: "Give 2 & possibly a third",
+    description: "You point at two persons and tell them to drink. You can also tell one person to take two drinks.",
+  },
+  "3": {
+    name: "Take 3",
+    description: "\"Three is me.\" You take a drink.",
+  },
+  "4": {
+    name: "Give 2 Take 2",
+    description: "You give out two drinks, and take two yourself.",
+  },
+  "5": {
+    name: "Rule",
+    description: "Set a rule to be followed, e.g. drink with your left hand, tap your head before you drink, don't use first names, etc.",
+  },
+  "6": {
+    name: "Thumbs",
+    description: "Place your thumb on the table, and try to do this without anyone noticing. As people notice, they will also have to put their thumb on the table too. The last person to place their thumb on the table drinks.",
+  },
+  "7": {
+    name: "Heaven",
+    description: "Last person to raise their hand will drink.",
+  },
+  "8": {
+    name: "Mate",
+    description: "Choose a person to be your mate and they drink when you drink for the rest of the game.",
+  },
+  "9": {
+    name: "Rhyme Time",
+    description: "\"Nine is Rhyme\". You say a word, and the person to your right has to say a word that rhymes. This continues around the table until someone cannot think of a word. This person must drink. The same word may not be used twice.",
+  },
+  "T": {
+    name: "Categories",
+    description: "You come up with a category of things, and the person to your right must come up with something that falls within that category. This goes on around the table until someone can't come up with anything. This person must drink.",
+  },
+  "J": {
+    name: "Guys Drink",
+    description: "All the guys at the table must take a drink.",
+  },
+  "Q": {
+    name: "Girls Drink",
+    description: "All the girls at the table must take a drink.",
+  },
+  "K": {
+    name: "King's Cup",
+    description: "When each of the first 3 Kings are drawn, the person who drew the card puts some of their drink into the King's Cup at the center of the table. When the 4th King is drawn, the person who drew the 4th King must drink the contents of the King's Cup.",
+  },
+};
